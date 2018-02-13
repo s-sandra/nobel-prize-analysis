442ae3a14403ec036dd1dfc0369988c328b902b1
Bradley Dufour <dufour.bradley@gmail.com>
2018-02-13 18:34:51 -0500

    adds data parsing for Question 3 (UNFINISHED)

--- challenge1.py
+++ challenge1.py
@@ -5,3 +5,17 @@ with open("nobel.json","r") as data:
     nobel_data = json.load(data)
 
 # format data into array for analysis.
+
+#%%-----
+#question3
+laureates = nobel_data['laureates']
+genders = []
+shares = []
+for laureate in laureates:
+    if(not(laureate['born'] == '0000-00-00' and laureate['died'] == '0000-00-00') and (laureate['gender'] == 'male' or laureate['gender'] == 'female')):
+        for x in range(len(laureate['prizes'])):
+            genders.append(laureate['gender'])
+        for prize in laureate['prizes']:
+            shares.append(laureate['prizes'])
+#shares = laureates[:]['prizes'][:]['share']
+print(genders)
