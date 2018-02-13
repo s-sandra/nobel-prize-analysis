08c7df7600eabea1945f3be40d80c686933f32c2
s-sandra <aleksandra.shtabnaya@gmail.com>
2018-02-13 13:53:02 -0500

    Created dataframe storing gender and year of laureates.

--- challenge1.py
+++ challenge1.py
@@ -1,7 +1,21 @@
 import json
+import pandas as pd
 
 # loads nobel prize data into nobel_data object
 with open("nobel.json","r") as data:
     nobel_data = json.load(data)
 
-# format data into array for analysis.
+# creates dataframe storing gender and year of award
+rows = []
+for laureate in nobel_data["laureates"]:
+
+    # each laureate is a dictionary
+    gender = laureate["gender"]
+
+    # checks for bogus entries
+    if laureate["born"] != "0000-00-00" and laureate["died"] != "0000-00-00":
+        year = laureate["prizes"][0]["year"]
+        # each prizes in laureate is a list containing a dictionary
+
+    rows.append({"gender" : gender, "year" : year})
+gender_and_year = pd.DataFrame(rows)
